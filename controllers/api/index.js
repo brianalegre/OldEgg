012a4fabdc1543dd45d09443c95a517c0d94553f
Axe <axearzadon@gmail.com>
2022-06-27 17:46:46 -0700

    Completed categories routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 //THIS index file is the entry point of our API(data) routes,
 //it will bring in all api routes and export 1 router middleware
 const router = require('express').Router();
-const exampleRoutes = require('./exampleAPIRoutes');
-router.use('/example', exampleRoutes);
+const categoriesRoutes = require('./categoriesRoutes');
+
+router.use('/categories', categoriesRoutes);
 
 module.exports = router;
