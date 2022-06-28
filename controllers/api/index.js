0cb6ead1d9fa42c4e02484527eca6dda6d32a39d
Brian Alegre <98508123+brianalegre@users.noreply.github.com>
2022-06-27 22:16:37 -0700

    Delete index.js
    Removing to avoid merge conflict

--- controllers/api/index.js
+++
@@ -1,8 +0,0 @@
-//THIS index file is the entry point of our API(data) routes,
-//it will bring in all api routes and export 1 router middleware
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-
-router.use('/', userRoutes);
-
-module.exports = router;
