535f452643c3103a4cf9a1d5ab1c656d8065d22c
Brian Alegre <brialegre@yahoo.com>
2022-06-30 20:53:50 -0700

    Able to do partials for categories

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const categoryHandlebar = require('./categoryHandlebar');
 
-router.use('/', homeRoutes);
+router.use('/', homeRoutes, categoryHandlebar);
 router.use('/api', apiRoutes);
+// router.use('/categories', categoryHandlebar);
 
 module.exports = router;
