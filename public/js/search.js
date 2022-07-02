1a3747e5a3d7b70140521a628fecba954007b7cf
Brian Alegre <brialegre@yahoo.com>
2022-07-01 22:49:08 -0700

    Changed queryString to a static url in search.js

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,27 @@
+// HTML Targeting Variables
+var searchKey = document.getElementById('searchText');
+
+// Function for searching Prodcuts
+function searchProducts() {
+  // Get Input Value
+  var searchInputVal = document.getElementById('searchText').value.trim();
+  var queryString = 'http://localhost:3001/api/search/' + searchInputVal;
+
+  // Go to next page
+  location.assign(queryString);
+}
+
+// Listen for Enter Key to be pressed
+searchKey.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    searchProducts();
+  }
+});
+
+
+
+
 // // API CALL
 // // Test API Call
 // const getProducts = async () => {
