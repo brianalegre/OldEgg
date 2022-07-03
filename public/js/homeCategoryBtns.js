5e6c2b36f7f71ff7f09bc2ff1e0736a98822c3ea
Axe714 <axearzadon@gmail.com>
2022-07-03 14:43:03 -0700

    Removed console log to clean up console

--- public/js/homeCategoryBtns.js
+++ public/js/homeCategoryBtns.js
@@ -4,7 +4,6 @@ const createBtns = async () => {
   for (let i = 0; i < categoryBtns.length; i++) {
     const categoriesAPI = await fetch('http://localhost:3001/api/categories');
     const categories = await categoriesAPI.json();
-    console.log(categories[i].category_name)
     categoryBtns[i].textContent = categories[i].category_name
     categoryBtns[i].addEventListener('click', () => {
         document.location.replace('/categories/' + categories[i].category_id)
