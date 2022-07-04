e091c4dd3586ddf898a2374d11ebc0a0a21e7221
Brian Alegre <brialegre@yahoo.com>
2022-07-04 16:40:22 -0700

    Debugging Seed and associations for productsCarts

--- seeds/carts-seeds.js
+++ seeds/carts-seeds.js
@@ -6,16 +6,16 @@ const cartsData = [
     product_id: 3,
     total_price: 199.5,
   },
-  {
-    user_id: 3,
-    product_id: 6,
-    total_price: 286.98,
-  },
-  {
-    user_id: 4,
-    product_id: 3,
-    total_price: 199.5,
-  },
+  // {
+  //   user_id: 1,
+  //   product_id: 6,
+  //   total_price: 286.98,
+  // },
+  // {
+  //   user_id: 1,
+  //   product_id: 3,
+  //   total_price: 199.5,
+  // },
 ];
 
 const seedCarts = () => Carts.bulkCreate(cartsData);
