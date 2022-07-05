f3f1dc0fd5b04c4ec665e2e07833ce4f1cf08f89
Tonyavall <tonyavallescas@gmail.com>
2022-07-05 15:49:26 -0700

    Stashing

--- controllers/cartHandlebar.js
+++ controllers/cartHandlebar.js
@@ -3,27 +3,30 @@ const { Carts, Products } = require('../models');
 
 router.get('/cart', async (req, res) => {
     try {
-      const cartData = await Carts.findOne({where: {user_id: req.session.user_id}})
+      const dbCartData = await Carts.findOne({where: {user_id: req.session.user_id}})
+      const cartProducts = dbCartData.map(product => product.get({plain:true}))
+
       res.render('cart', {
-        cartData,
+        cartProducts,
         logged_in: req.session.logged_in
       })
     } catch (err) {
       console.log(err)
+      res.status(500).json(err);
     }
 });
 
-router.post('/cart', async (req, res) => {
-    try {
-      const currentProduct = await Products.findOne({where: {product_id: req.body.productId}})
-      const cartData = await Carts.findOne({where: {user_id: req.session.user_id}})
-      if (cartData === null) {
-        Carts.create
-      }
-    //   console.log(currentProduct, 'THIS IS CURRENT PRODUCT!!!!')
-    } catch (err) {
+// router.post('/cart', async (req, res) => {
+//     try {
+//       const currentProduct = await Products.findOne({where: {product_id: req.body.productId}})
+//       const cartData = await Carts.findOne({where: {user_id: req.session.user_id}})
+//       if (cartData === null) {
+//         // Carts.create
+//       }
+//     //   console.log(currentProduct, 'THIS IS CURRENT PRODUCT!!!!')
+//     } catch (err) {
 
-    }
-});
+//     }
+// });
 
 module.exports = router;
