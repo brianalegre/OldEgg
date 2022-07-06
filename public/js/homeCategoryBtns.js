e9cfd338aaa33b0bd5a9090174f4658d4becbe98
Axe <axearzadon@gmail.com>
2022-07-05 19:16:27 -0700

    Changed nav bar icons

--- public/js/homeCategoryBtns.js
+++ public/js/homeCategoryBtns.js
@@ -4,10 +4,10 @@ const createBtns = async () => {
   for (let i = 0; i < categoryBtns.length; i++) {
     const categoriesAPI = await fetch('/api/categories');
     const categories = await categoriesAPI.json();
-    categoryBtns[i].textContent = categories[i].category_name
+    categoryBtns[i].textContent = categories[i].category_name;
     categoryBtns[i].addEventListener('click', () => {
-        document.location.replace('/categories/' + categories[i].category_id)
-    })
+      document.location.replace('/categories/' + categories[i].category_id);
+    });
   }
 };
 
