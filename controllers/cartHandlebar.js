34b89305aaa446adb80cb779f96e46761befc601
Tonyavall <tonyavallescas@gmail.com>
2022-07-06 18:23:19 -0700

    Updated checkoutFromCart functionality. Functionality is done, just need to add bare html/css

--- controllers/cartHandlebar.js
+++ controllers/cartHandlebar.js
@@ -1,6 +1,10 @@
 const router = require('express').Router();
 const { Carts, Products } = require('../models');
 const loggedIn = require('../utils/auth');
+const checkoutHandlebar = require('./checkoutHandlebar')
+
+// For checkout route = /cart/checkout
+router.use('/cart', checkoutHandlebar)
 
 router.get('/cart', loggedIn, async (req, res) => {
   try {
@@ -12,10 +16,17 @@ router.get('/cart', loggedIn, async (req, res) => {
     const dbProductInfo = await Products.findAll({where: {product_id: cartProductsIds}})
     const cartProducts = dbProductInfo.map(product => product.get({plain:true}))
     const cartProdLength = cartProducts.length
-    
+
+    const hasItems = cartProdLength > 0 ? true : false
+    const subTotal = cartProducts.map(product=> {
+      return parseInt(product.price)
+    }).reduce((a,b) => a+b, 0)
+
     res.render('cart', {
       cartProducts,
       cartProdLength,
+      subTotal,
+      hasItems,
       logged_in: req.session.logged_in
     })
   } catch (err) {
