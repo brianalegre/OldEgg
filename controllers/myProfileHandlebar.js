7d4a07aa2846f778734d484ff425745f11c16009
Brian Alegre <brialegre@yahoo.com>
2022-07-06 20:47:41 -0700

    Added logic updating profile information

--- controllers/myProfileHandlebar.js
+++ controllers/myProfileHandlebar.js
@@ -1,36 +1,13 @@
 const router = require('express').Router();
 const { Users } = require('../models');
 
-
 //end point of /profile
-// router.get('/profile/:id', async (req, res) => {
-//   try {
-//     const userById = await Users.findByPk(req.params.id, {
-//       include: [{ all: true, nested: true }],
-//     });
-//     if (!userById) {
-//       return res.status(404).json({
-//         message: 'This user ID does not exist. Please enter a valid user ID!',
-//       });
-//     }
-//     const user = userById.get({ plain: true });
-//     res.render('myProfile', {
-//       user,
-//       logged_in: req.session.logged_in
-//     });
-//   } catch (err) {
-//     return res.status(500).json(err);
-//   }
-// });
-
-
 router.get('/profile', async (req, res) => {
   try {
     const userData = await Users.findOne({
       where: {
         user_id: req.session.user_id
       }
-      // , include: [{ all: true, nested: true }]
     });
 
     if (!userData) {
@@ -42,14 +19,13 @@ router.get('/profile', async (req, res) => {
     const users = userData.get({ plain: true });
     res.render('myProfile', {
       users,
-      // logged_in: req.session.logged_in
+      logged_in: req.session.logged_in
     });
   } catch (err) {
     return res.status(500).json(err);
   }
 }
 );
-
 
 
 module.exports = router;
--- public/js/myProfile.js
+++ public/js/myProfile.js
@@ -8,29 +8,91 @@ const updateBtn = document.querySelector('#update-btn');
 
 // Check if password and confirm password match
 
-// const first_name = document.querySelector('#firstname-signup').value.trim();
-// const last_name = document.querySelector('#lastname-signup').value.trim();
-// const email = document.querySelector('#email-signup').value.trim();
-const password = document.querySelector('#password-login').value.trim();
-const confirmPassword = document.querySelector('#password-confirm').value.trim();
 
-function checkPassword() {
+
+// function checkPassword() {
+
+const updateUserInfo = async (event) => {
+  event.preventDefault();
+
+  // HTML Targeting Elements
+  const password = document.querySelector('#password-login').value.trim();
+  const confirmPassword = document.querySelector('#password-confirm').value.trim();
+  const first_name = document.querySelector('#update-firstname').value.trim();
+  const last_name = document.querySelector('#update-lastname').value.trim();
+  const email = document.querySelector('#update-email').value.trim();
+
   // Check if password and confirm password match
-  if (password !== confirmPassword) {
+  if (password === confirmPassword) {
+    console.log('Password match');
+  } else {
     const message = {
       tag: 'p',
       setAttr: {
         class: 'invalid-auth',
       },
-      textContent: 'Passwords do not match!',
+      // Display message on page
+      textContent: 'Passwords do NOT match!',
       appendTo: userUpdateForm,
     };
     appendContent(message);
-  } else {
-    console.log('passwords match');
   }
-}
+
+  // Check if email is valid
+  if (email.includes('@') && email.includes('.')) {
+    console.log('Email is valid');
+  } else {
+    const message = {
+      tag: 'p',
+      setAttr: {
+        class: 'invalid-auth',
+      },
+      // Display message on page
+      textContent: 'Email is invalid!',
+      appendTo: userUpdateForm,
+    };
+    appendContent(message);
+  }
+
+  // Check if first_name and last_name are valid
+  if (first_name && last_name) {
+    console.log('First and last name are valid');
+  } else {
+    const message = {
+      tag: 'p',
+      setAttr: {
+        class: 'invalid-auth',
+      },
+      // Display message on page
+      textContent: 'First or last name are invalid!',
+      appendTo: userUpdateForm,
+    };
+    appendContent(message);
+  }
+  // Send updated data to backend
+  try {
+    const response = await fetch('/api/users', {
+      method: 'PUT',
+      body: JSON.stringify({ username, first_name, last_name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+    const loggedIn = await response.json();
+    if (!loggedIn) {
+      console.log('something went wrong');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
 
 
 // Listen for submit event on update button
-updateBtn.addEventListener('click', checkPassword);
+// updateBtn.addEventListener('click', function (event) {
+//   event.preventDefault();
+//   checkPassword();
+// });
+
+updateBtn.addEventListener('click', updateUserInfo);
