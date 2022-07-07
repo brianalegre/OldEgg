f9a5863b83b597dd49d378f8726f80d682507728
Brian Alegre <brialegre@yahoo.com>
2022-07-06 19:03:51 -0700

    Testing routes and handlebars for users logged in

--- controllers/myProfileHandlebar.js
+++ controllers/myProfileHandlebar.js
@@ -3,24 +3,46 @@ const { Users } = require('../models');
 
 
 //end point of /profile
-router.get('/profile/:id', async (req, res) => {
+// router.get('/profile/:id', async (req, res) => {
+//   try {
+//     const userById = await Users.findByPk(req.params.id, {
+//       include: [{ all: true, nested: true }],
+//     });
+//     if (!userById) {
+//       return res.status(404).json({
+//         message: 'This user ID does not exist. Please enter a valid user ID!',
+//       });
+//     }
+//     const user = userById.get({ plain: true });
+//     res.render('myProfile', {
+//       user,
+//       logged_in: req.session.logged_in
+//     });
+//   } catch (err) {
+//     return res.status(500).json(err);
+//   }
+// });
+
+
+router.get('/profile', async (req, res) => {
   try {
-    const userById = await Users.findByPk(req.params.id, {
-      include: [{ all: true, nested: true }],
-    });
-    if (!userById) {
+    const userData = await Users.findAll({ where: { user_id: req.session.user_id } });
+
+    if (!userData) {
       return res.status(404).json({
         message: 'This user ID does not exist. Please enter a valid user ID!',
       });
     }
-    const user = userById.get({ plain: true });
-    res.render('myProfile', {
-      user,
+    // res.status(200).json(user);
+    const users = userData.get({ plain: true });
+    res.render('myprofile', {
+      users,
       logged_in: req.session.logged_in
     });
   } catch (err) {
     return res.status(500).json(err);
   }
-});
+}
+);
 
 module.exports = router;
