7bda5919fc91dea3f2748e111e9283ba84daa717
Axe <axearzadon@gmail.com>
2022-07-07 21:19:01 -0700

    oops:

--- public/js/checkoutFromCart.js
+++ public/js/checkoutFromCart.js
@@ -1,24 +1,51 @@
-const paymentCheckoutBtn = document.getElementById('payment-checkout-btn')
-const btnMsgContainer = document.querySelector('.btn-msg-container')
+const paymentCheckoutBtn = document.getElementById('payment-checkout-btn');
+const btnMsgContainer = document.querySelector('.btn-msg-container');
 
 const checkoutFromCart = async () => {
-    const response = await fetch('/cart/checkout', {method: 'POST'})
+  try {
+    const response = await fetch('/cart/checkout', { method: 'POST' });
     if (response.ok) {
-        return document.location.replace('/cart')
+      return document.location.replace('/cart');
     }
-    // otherwise if the res is not ok
-    const message = {
-        tag: 'p',
-        setAttr: {
-          class: 'invalid-auth',
-        },
-        // set this to backend message
-        textContent: 'Insufficient balance',
-        appendTo: btnMsgContainer,
-      };
-    appendContent(message)
-}
+    const data = await response.json();
+    const checkMessage = document.querySelector('.invalid-auth');
+    if (checkMessage) {
+      checkMessage.remove();
+    }
+    // error handling
+    switch (data.error) {
+      case 0:
+        message = {
+          tag: 'p',
+          setAttr: {
+            class: 'invalid-auth',
+          },
+          // set this to backend message
+          textContent: data.message,
+          appendTo: btnMsgContainer,
+        };
+        appendContent(message);
+        break;
+      case 1:
+        message = {
+          tag: 'p',
+          setAttr: {
+            class: 'invalid-auth',
+          },
+          // set this to backend message
+          textContent: data.message,
+          appendTo: btnMsgContainer,
+        };
+        appendContent(message);
+        break;
+      default:
+        break;
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 if (paymentCheckoutBtn) {
-    paymentCheckoutBtn.addEventListener('click', checkoutFromCart)
+  paymentCheckoutBtn.addEventListener('click', checkoutFromCart);
 }
